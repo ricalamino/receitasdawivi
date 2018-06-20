de447cd2acff3299b7621876c82deffad56d832d
Ricardo Alamino <ricardo.alamino@bxchain.io>
2018-06-19 21:27:20 -0300

    Dot files

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Receitas da Wivian',
+  description: 'Lista de receitas',
+  base: '/receitas/',
+  serviceWorker: true,
+  head: [
+    ['meta', { name: 'theme-color', content: '#2196f3' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+  ]
+}
