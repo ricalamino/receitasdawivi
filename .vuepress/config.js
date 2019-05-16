58fb370cd6b57eeb207dffde7deb172426535cfb
Ricardo Alamino <ricardo.alamino@bxchain.io>
2019-05-16 17:17:00 -0300

    base changed to /

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Receitas da Wivian',
   description: 'Lista de receitas',
-  base: '/receitas/',
+  base: '/',
   serviceWorker: true,
   head: [
     ['meta', { name: 'theme-color', content: '#2196f3' }],
